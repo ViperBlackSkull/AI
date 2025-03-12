19e66ca3a0e8518789fadc86f3a90710285238f5
Viper <vipergrosman@hotmail.com>
2025-03-12 08:21:14 -0400

    Create playwright.sh

---
+++ OpenManus/playwright.sh
@@ -0,0 +1,5 @@
+# Needed to run on arch
+sudo ln -s /usr/lib/libffi.so /usr/lib/libffi.so.7
+sudo ln -s /usr/lib/libicudata.so /usr/lib/libicudata.so.66
+sudo ln -s /usr/lib/libicui18n.so /usr/lib/libicui18n.so.66
+sudo ln -s /usr/lib/libicuuc.so /usr/lib/libicuuc.so.66
